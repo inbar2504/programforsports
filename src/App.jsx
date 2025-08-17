c561d0f42e6fe1736569666ac23f5fa039365d31
a b <ab@example.com>
2025-08-17 14:19:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StartPage from "./startPage";
+import ImportantNotes from "./ImportantNotes";
+import NeedsPage from "./needPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-Content">
+          <Routes>
+             <Route path="/" element={<StartPage />} />
+             <Route path="/important-note" element={<ImportantNotes />} />
+             <Route path="/need-page" element={<NeedsPage/>} />
+          </Routes>
+          
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
