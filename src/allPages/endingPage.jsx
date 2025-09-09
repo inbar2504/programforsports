7b21fa7daa4e179bf0449b006d4f8efc1b6c24db
a b <ab@example.com>
2025-09-09 15:23:37 +0300

    added back button for last page

--- src/allPages/endingPage.jsx
+++ src/allPages/endingPage.jsx
@@ -3,8 +3,12 @@ import cat from "../media/cat.svg";
 import kanaf from "../media/kanaf.svg";
 import school from "../media/school.svg";
 import shaldag from "../media/shaldag.svg";
+import forwardBtn from "../media/forwardBtn.svg";
+import backwardBtn from "../media/backwardBtn.svg";
+import { useNavigate } from "react-router-dom";
 
 const EndingPage = () => {
+    const navigate = useNavigate();
   return (
     <div className="ending-page-content">
       <div className="logos">
@@ -17,6 +21,24 @@ const EndingPage = () => {
         <div className="text ending-page-title">כל הכבוד<br></br> סיימתם את הלומדה!</div>
         <div className="text ending-small-message">נתראה במועד הגיוס...</div>
       </div>
+      <div className="buttons-move">
+              <img
+                src={forwardBtn}
+                alt="back"
+                onClick={() => {
+                  navigate("/start-programs")
+                }}
+                className="move-button"
+                id="forward-btn"
+              />
+              <img
+                src={backwardBtn}
+                alt="next"
+                className="move-button"
+                id="backward-btn"
+                style={{ visibility: "hidden"}}
+              />
+            </div>
     </div>
   );
 };
